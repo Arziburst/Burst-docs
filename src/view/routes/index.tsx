d174e76d31568e213c93466f911e71de3385ce75
Belartale <belartale@gmail.com>
2022-10-19 11:57:20 +0300

    make center of screen for spinner

--- src/view/routes/index.tsx
+++ src/view/routes/index.tsx
@@ -1,5 +1,6 @@
 // Core
 import React, { FC, Suspense } from 'react';
+import { ContainerSetHeightWithoutHeader } from '../containers';
 
 // Elements
 import { Spinner } from '../elements';
@@ -9,7 +10,11 @@ import { Public } from './Public';
 
 export const Routes: FC = () => {
     return (
-        <Suspense fallback = { <Spinner /> }>
+        <Suspense fallback = {
+            <ContainerSetHeightWithoutHeader>
+                <Spinner />
+            </ContainerSetHeightWithoutHeader>
+        }>
             <Public />
         </Suspense>
     );
